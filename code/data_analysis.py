33bd452c20a334500c150e0ed88bbb80e85e7cdc
Williamsillon <williamgleason5@gmail.com>
2025-10-15 17:49:32 -0500

    connecting clean.py to data_analysis.py

--- code/data_analysis.py
+++ code/data_analysis.py
@@ -1,16 +1,25 @@
 import pandas as pd
 import statsmodels.api as sm
-import matplotlib.pyplot as plt
-import numpy as np
+from clean import process_csvs, csv_info
 
-df.info()
-df.describe()
+yearly_dataframes = process_csvs(csv_info)
+
+df_price = yearly_dataframes[0].rename(columns={'Average': 'Price_per_Barrel'})
+df_production = yearly_dataframes[1].rename(columns={'Average': 'Production_Barrels'})
+df_wells = yearly_dataframes[2].rename(columns={'Average': 'Oil_Well_Count'})
+df_gdp = yearly_dataframes[3].rename(columns={'Average': 'Texas_GDP'})
+
+dfs = [df_price, df_production, df_wells, df_gdp]
+df = dfs[0]
+for other_df in dfs[1:]:
+    df = df.merge(other_df, on='Year', how='inner')
+
+print(df.info())
+print(df.describe())
 
 y = df['Price_per_Barrel']
-
-X = ['Production_Barrels', 'Oil_Well_Count', 'Texas_GDP']
-
-X = sm.add_constant(df[X])
+X = df[['Production_Barrels', 'Oil_Well_Count', 'Texas_GDP']]
+X = sm.add_constant(X)
 
 model = sm.OLS(y, X).fit()
 print(model.summary())
