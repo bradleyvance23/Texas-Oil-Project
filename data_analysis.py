5cd630f6a51e777ff1f588cd46279090d9299ce0
Bradley Vance <bradleyvance@wireless-10-147-85-70.public.utexas.edu>
2025-10-15 16:01:58 -0500

    Added code to run individual regressions on price using other regressions

--- data_analysis.py
+++ data_analysis.py
@@ -6,11 +6,21 @@ import numpy as np
 df.info()
 df.describe()
 
-y = ['Price_per_Barrel']
+y = df['Price_per_Barrel']
 
 X = ['Production_Barrels', 'Oil_Well_Count', 'Texas_GDP']
 
 X = sm.add_constant(df[X])
 
-model = sm.OLS(df[y], X).fit()
+model = sm.OLS(y, X).fit()
 print(model.summary())
+
+print('\n' + '='*60 + '\n')
+
+for var in ['Production_Barrels', 'Oil_Well_Count', 'Texas_GDP']:
+    Z = df[[var]]
+    Z = sm.add_constant(Z)
+    model = sm.OLS(y, Z).fit()
+    print(f'Regression: Price_per_Barrel ~ {var}')
+    print(model.summary())
+    print('\n' + '='*60 + '\n')
