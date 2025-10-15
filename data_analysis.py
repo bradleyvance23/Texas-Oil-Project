250ab1a206e12f99fc838a8b701e42fd7b1b92b5
Bradley Vance <bradleyvance@wireless-10-147-85-70.public.utexas.edu>
2025-10-15 15:35:11 -0500

    Added initial code for  data analysis file

--- data_analysis.py
+++ data_analysis.py
@@ -1,7 +1,16 @@
 import pandas as pd
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
+import numpy as np
 
-def load_data(file_path):
-    return df
+df.info()
+df.describe()
 
+y = ['Price_per_Barrel']
+
+X = ['Production_Barrels', 'Oil_Well_Count', 'Texas_GDP']
+
+X = sm.add_constant(df[X])
+
+model = sm.OLS(df[y], X).fit()
+print(model.summary())
