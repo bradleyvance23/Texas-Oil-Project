448093c5d10f99716b6a5b7f0acbc83aa5f6feca
Williamsillon <williamgleason5@gmail.com>
2025-10-15 16:47:29 -0500

    succesful check

--- code/clean.py
+++ code/clean.py
@@ -4,31 +4,34 @@ import pandas as pd
 script_dir = os.path.dirname(os.path.abspath(__file__))
 csv_files = [
     os.path.join(script_dir, '../artifacts/monthly_oil_prices.csv'),
-    os.path.join(script_dir, '../artifacts/texas_well_counts.csv'),
     os.path.join(script_dir, '../artifacts/monthly_oil_production.csv'),
+    os.path.join(script_dir, '../artifacts/texas_well_counts.csv'),
     os.path.join(script_dir, '../artifacts/TX_OIL_GDP.csv'),
 ]
 
-def dataframes_and_averages(csv_paths, year_col='Year', value_col='Value', monthly_sep='-'):
+csv_info = [
+    {'file': csv_files[0], 'type': 'monthly', 'month_cols': ['January','February','March','April','May','June','July','August','September','October','November','December']},
+    {'file': csv_files[1], 'type': 'monthly', 'month_cols': ['January','February','March','April','May','June','July','August','September','October','November','December']},
+    {'file': csv_files[2], 'type': 'yearly', 'value_col': 'Well_Count'},
+    {'file': csv_files[3], 'type': 'yearly', 'value_col': 'GDP (Millions of Dollars)'}
+]
 
+def process_csvs(csv_info):
     yearly_dfs = []
     
-    for path in csv_paths:
-        df = pd.read_csv(path)
+    for info in csv_info:
+        df = pd.read_csv(info['file'])
         
-        if monthly_sep in str(df[year_col].iloc[0]):
-            df['Year'] = df[year_col].str.split(monthly_sep).str[0].astype(int)
-            yearly_avg = df.groupby('Year')[value_col].mean().reset_index()
-            yearly_avg.rename(columns={value_col: 'Average'}, inplace=True)
+        if info['type'] == 'monthly':
+            df['Average'] = df[info['month_cols']].mean(axis=1)
+            yearly_avg = df[['Year', 'Average']]
+        elif info['type'] == 'yearly':
+            yearly_avg = df[['Year', info['value_col']]].rename(columns={info['value_col']: 'Average'})
         else:
-            df['Year'] = df[year_col].astype(int)
-            yearly_avg = df[[year_col, value_col]].rename(columns={year_col: 'Year', value_col: 'Average'})
+            raise ValueError("CSV type must be 'monthly' or 'yearly'")
         
         yearly_dfs.append(yearly_avg)
     
     return yearly_dfs
 
-yearly_dataframes = dataframes_and_averages(csv_files, year_col='Date', value_col='Production')
-
-for df in yearly_dataframes:
-    print(df.head())
+yearly_dataframes = process_csvs(csv_info)
