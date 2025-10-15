aebcb4b583e961f1b44b1a00038768c162ae95e1
Bradley Vance <bradleyvance@wireless-10-147-85-70.public.utexas.edu>
2025-10-15 17:15:13 -0500

    Added back the main scrape file

---
+++ code/main_scrape_file.py
@@ -0,0 +1,17 @@
+import scrape_barrel_production
+import scrape_texas_gdp
+import scrape_well_count_new
+import texas_oil_prices
+
+def run_all_scraping():
+    print("Running scraping files\n")
+
+    scrape_barrel_production.main()
+    scrape_texas_gdp.main()
+    scrape_well_count.main()
+    texas_oil_prices.main()
+
+    print("\n Scraping complete")
+
+if __name__ == "__main__":
+    run_all_scraping()
