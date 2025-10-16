38aaa682bac2ea76ee680e001b2a259ef755a344
Williamsillon <williamgleason5@gmail.com>
2025-10-15 20:40:44 -0500

    corrected image name

--- code/data_visualization.py
+++ code/data_visualization.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from data_analysis import df
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-set_path = os.path.join(script_dir, '../artifacts/texas_oil_price_time_series.png')
+set_path = os.path.join(script_dir, '../artifacts/texas_oil_price_actual_vs_predicted.png')
 
 plt.figure(figsize=(10,6))
 plt.plot(df['Year'], df['Price_per_Barrel'], marker='o', label='Actual', color='blue')
