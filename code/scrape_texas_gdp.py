af0191a3a6e7c42ed9bca9fd9e72e44815af6a11
Bradley Vance <bradleyvance@wireless-10-147-85-70.public.utexas.edu>
2025-10-15 20:10:59 -0500

    Fixed texas gdp scrape file

--- code/scrape_texas_gdp.py
+++ code/scrape_texas_gdp.py
@@ -3,48 +3,50 @@ import os
 import csv
 from dotenv import load_dotenv
 
-load_dotenv()
-FRED_API_KEY = os.getenv("FRED_API_KEY")
-if not FRED_API_KEY:
-	raise RuntimeError("FRED_API_KEY not set. Create a .env file with FRED_API_KEY=your_key")
+def main():
 
-SERIES_ID = "TXPETCOALMANNGSP"
-URL = "https://api.stlouisfed.org/fred/series/observations"
-
-params = {
-    "series_id": SERIES_ID,
-    "api_key": FRED_API_KEY,
-    "file_type": "json"
-}
-
-def get_fred_gdp(url, params):
-	response = requests.get(url, params=params)
-	response.raise_for_status()
+	FRED_API_KEY = "c4dd8f412e6a4f35c90c07a8c0292de3"
 	
-	return response.json()
+	SERIES_ID = "TXPETCOALMANNGSP"
+	URL = "https://api.stlouisfed.org/fred/series/observations"
 
-fred_json = get_fred_gdp(URL,params)
+	params = {
+		"series_id": SERIES_ID,
+		"api_key": FRED_API_KEY,
+		"file_type": "json"
+	}
 
-def parse_fred_data(json_data):
-	rows = []
-	for obs in json_data.get("observations", []):
-		date = obs.get("date")
-		value = obs.get("value")
-		if value == ".":
-			continue
-		rows.append({ 
-			"Year":date,
-			"GDP (Millions of Dollars)":value 
-		})
-	return rows 
+	def get_fred_gdp(url, params):
+		response = requests.get(url, params=params)
+		response.raise_for_status()
+		
+		return response.json()
 
-def write_gdp_csv(data, filename="TX_OIL_GDP.csv"):
-	fieldnames = ("Year", "GDP (Millions of Dollars)")
+	fred_json = get_fred_gdp(URL,params)
 
-	with open(filename, "w", newline="") as f:
-		writer = csv.DictWriter(f, fieldnames=fieldnames)
-		writer.writeheader()
-		writer.writerows(data)
+	def parse_fred_data(json_data):
+		rows = []
+		for obs in json_data.get("observations", []):
+			date = obs.get("date")
+			value = obs.get("value")
+			if value == ".":
+				continue
+			rows.append({ 
+				"Year":date,
+				"GDP (Millions of Dollars)":value 
+			})
+		return rows 
 
-texas_oil_gdp = parse_fred_data(fred_json)
-write_gdp_csv(texas_oil_gdp)
+	def write_gdp_csv(data, filename="TX_OIL_GDP.csv"):
+		fieldnames = ("Year", "GDP (Millions of Dollars)")
+
+		with open(filename, "w", newline="") as f:
+			writer = csv.DictWriter(f, fieldnames=fieldnames)
+			writer.writeheader()
+			writer.writerows(data)
+
+	texas_oil_gdp = parse_fred_data(fred_json)
+	write_gdp_csv(texas_oil_gdp)
+
+if __name__ == "__main__":
+    main()
