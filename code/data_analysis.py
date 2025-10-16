c95d0fdb3704143b2a24c47ac0d9e64ded5438f8
Blayne Burke <blayneburke@wireless-10-147-46-2.public.utexas.edu>
2025-10-15 21:53:47 -0500

    fix README file and make html of regression results.

--- code/data_analysis.py
+++ code/data_analysis.py
@@ -27,6 +27,9 @@ df['Predicted_Price'] = model.predict(X)
 
 print('\n' + '='*60 + '\n')
 
+with open("artifacts/regression_summary.html", "w") as f:
+    f.write(model.summary().as_html())
+
 for var in ['Production_Barrels', 'Oil_Well_Count', 'Texas_GDP']:
     Z = df[[var]]
     Z = sm.add_constant(Z)
